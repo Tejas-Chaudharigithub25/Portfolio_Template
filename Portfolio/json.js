664662c6032e04eb9169723ebd899c46b22a1229
Tejas-Chaudharigithub25 <103263313+Tejas-Chaudharigithub25@users.noreply.github.com>
2025-02-16 11:41:29 +0530

    portfolio template

---
+++ Portfolio/json.js
@@ -0,0 +1,18 @@
+const skills = [
+    {
+        titleKey: 'Programming Language',
+        images: ['p2.jpg', 'p2.jpg']
+    },
+    {
+        titleKey: 'Programming Language',
+        images: ['p2.jpg', 'p2.jpg']
+    },
+    {
+        titleKey: 'Programming Language',
+        images: ['p2.jpg', 'p2.jpg']
+    },
+    {
+        titleKey: 'Programming Language',
+        images: ['p2.jpg', 'p2.jpg']
+    }
+]
